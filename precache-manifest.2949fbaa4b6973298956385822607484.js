486e23920c3e467605cbb73886cde40db90118a5
Samul <samulshrestha101@gmail.com>
2020-07-26 15:17:23 +0545

    Updates

---
+++ precache-manifest.2949fbaa4b6973298956385822607484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715cd0d20b76d1b18128a85f257ea5e6",
+    "url": "/covid-tracker-v2/index.html"
+  },
+  {
+    "revision": "9b7e660deb54db50f37e",
+    "url": "/covid-tracker-v2/static/css/main.44858531.chunk.css"
+  },
+  {
+    "revision": "d3326ac1dda29d879af3",
+    "url": "/covid-tracker-v2/static/js/2.045d322e.chunk.js"
+  },
+  {
+    "revision": "14ed076e8c9c3d038ba497650386b32b",
+    "url": "/covid-tracker-v2/static/js/2.045d322e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7e660deb54db50f37e",
+    "url": "/covid-tracker-v2/static/js/main.1d5d9d31.chunk.js"
+  },
+  {
+    "revision": "c577b3aecf388370af2c",
+    "url": "/covid-tracker-v2/static/js/runtime-main.ffb58d43.js"
+  }
+]);
