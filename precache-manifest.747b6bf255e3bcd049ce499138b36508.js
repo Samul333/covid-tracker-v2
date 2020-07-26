57df889855798ee69ebef5821d6a5640b88d12ec
Samul <samulshrestha101@gmail.com>
2020-07-26 16:57:56 +0545

    Updates

--- precache-manifest.747b6bf255e3bcd049ce499138b36508.js
+++ precache-manifest.747b6bf255e3bcd049ce499138b36508.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3bd2f68ea1290ab42201e6183c9786b5",
+    "revision": "53f4c4d71f4757a64815828f4505ea15",
     "url": "/covid-tracker-v2/index.html"
   },
   {
-    "revision": "fa6eb99c6d841e2edcc5",
+    "revision": "b12122d7fa9ae5a9734d",
     "url": "/covid-tracker-v2/static/css/2.7c35e809.chunk.css"
   },
   {
-    "revision": "b25b3a982fa4b249bdf7",
+    "revision": "e38ec697f8da4c31fe14",
     "url": "/covid-tracker-v2/static/css/main.573fd4ec.chunk.css"
   },
   {
-    "revision": "fa6eb99c6d841e2edcc5",
-    "url": "/covid-tracker-v2/static/js/2.766a0dc5.chunk.js"
+    "revision": "b12122d7fa9ae5a9734d",
+    "url": "/covid-tracker-v2/static/js/2.5dc846bb.chunk.js"
   },
   {
     "revision": "68eb484dce045119a52132a8eb902e52",
-    "url": "/covid-tracker-v2/static/js/2.766a0dc5.chunk.js.LICENSE.txt"
+    "url": "/covid-tracker-v2/static/js/2.5dc846bb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b25b3a982fa4b249bdf7",
-    "url": "/covid-tracker-v2/static/js/main.a32f2700.chunk.js"
+    "revision": "e38ec697f8da4c31fe14",
+    "url": "/covid-tracker-v2/static/js/main.eb4ff757.chunk.js"
   },
   {
     "revision": "c577b3aecf388370af2c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-tracker-v2/precache-manifest.688a3daf5ad13b93a7a5dd0480620edc.js"
+  "/covid-tracker-v2/precache-manifest.747b6bf255e3bcd049ce499138b36508.js"
 );
 
 self.addEventListener('message', (event) => {
